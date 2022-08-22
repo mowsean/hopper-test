c5f8ae4f025f1e85d358764b0080a56be017f26a
mowsean@yahoo.com <mowsean@yahoo.com>
2022-08-22 12:52:48 +0000

    initial commit

---
+++ testhopper.py
@@ -0,0 +1,49 @@
+# Databricks notebook source
+import pyspark.sql.functions as F
+import pyspark.sql.types
+import pyspark.sql.utils
+from datetime import datetime, timedelta, date
+
+from  pyspark.sql.types import StructType, StructField, TimestampType, StringType, MapType, ArrayType, TimestampType
+from pyspark.sql.window import Window
+import pytz
+
+runDebugCommands = True
+
+tzLA = pytz.timezone('America/Los_Angeles') 
+
+# COMMAND ----------
+
+# MAGIC %run "/Managed Shares/_Libraries/Hopper Helper and Common Code"
+
+# COMMAND ----------
+
+HH = HopperHelper()
+
+# COMMAND ----------
+
+sFilter = "MachineName='TKDDEVMOHSIN'"
+sDate = datetime(2022,7,19)
+eDate = datetime(2022,7,22, 22, 0, 0)
+
+# COMMAND ----------
+
+# use this line below for specific dates
+dfRawEvents = HH.loadData(dataSource=HopperDataSource.Env08_RawEventJson, startDate=sDate, endDate=eDate, optionalFilterString="")
+dfEventsFiltered = dfRawEvents.filter(sFilter).orderBy(F.desc("UtcTimeStamp"))
+dfEventsFiltered = dfEventsFiltered.withColumn("TemplateTypeCode",F.col("Details.Parameters.TemplateTypeCode"))
+
+
+rawCount = dfRawEvents.count()
+inputRecordCount = dfEventsFiltered.count()
+print("Raw Counts: " + "{:,}".format(rawCount))
+print("Counts: " + "{:,}".format(inputRecordCount))
+
+
+# COMMAND ----------
+
+dfGrouped = dfEventsFiltered.groupBy("TemplateTypeCode").count()\
+  .withColumn('total', F.sum('count').over(Window.partitionBy()))\
+  .withColumn('percent', F.round(F.col('count')/F.col('total')*100,1))
+
+display(dfGrouped.orderBy(F.desc("count")))
